865b64941f1603ca341acdf06fbc018cfd0c9561
mbchavez27 <epikstrykermaxchavez23@gmail.com>
2025-03-12 10:05:16 +0800

    refactor: remake all c and add entry

--- CHAVEZ_CORDEZ_MP.C
+++ CHAVEZ_CORDEZ_MP.C
@@ -1,201 +1,139 @@
 #include <stdio.h>
 #include <string.h>
-#define MAXLETTERS 20
+#include "Headers.h"
 
-typedef char String20[21];
-
-#define MAXPAIRS 10
-#define MAXENTRY 150
-
-struct EntryPair
+int SearchEntryPair(EntryPairTag EntryPair[], int nEntryPairs, String20 language, String20 translation)
 {
-  int EntryPairKey;
-  String20 language;
-  String20 translation;
-};
+  int i = 0;
+  int index = -1;
 
-struct EntryList
+  for (i = 0; i < nEntryPairs; i++)
+  {
+    if (strcmp(EntryPair[i].language, language) == 0 && strcmp(EntryPair[i].translation, translation) == 0)
+      index = i;
+  }
+  return index;
+}
+
+int SearchEntry(EntryTag Entries[], int nEntry, String20 language, String20 translation, int *EntryIndex)
 {
-  int EntryListKey;
-  EntryPair entries[MAXPAIRS];
-  int nEntryPairs;
-};
+  int i = 0;
+  int index = -1;
 
-void DisplayEntries(EntryList *Entry, int nEntry)
+  for (i = 0; i < nEntry; i++)
+  {
+    if (SearchEntryPair(Entries[i].EntryPair, Entries[i].nEntryPairs, language, translation) != -1)
+    {
+      index = i;
+    }
+  }
+
+  return index;
+}
+
+void AddEntry(EntryTag *Entry, int *nEntry, EntryTag Entries[])
+{
+  int nEntryPairs = Entry->nEntryPairs;
+  String20 language, translation;
+
+  printf("Add Language: ");
+  scanf("%s", language);
+  printf("Add Translation: ");
+  scanf("%s", translation);
+  printf("\n\n");
+
+  int EntryPairIndex = 0;
+
+  if (SearchEntry(Entries, *nEntry, language, translation, &EntryPairIndex) != -1)
+  {
+    int input;
+    printf("Given Entry: %s: %s\n", language, translation);
+    printf("Entry Exists: %s: %s\n", language, translation);
+
+    printf("New Entry?[0/1]: ");
+    scanf("%d", &input);
+    if (input == 1)
+    {
+      strcpy(Entry->EntryPair[nEntryPairs].translation, translation);
+      strcpy(Entry->EntryPair[nEntryPairs].language, language);
+      Entry->nEntryPairs++;
+      (*nEntry)++;
+    }
+  }
+  else
+  {
+    strcpy(Entry->EntryPair[nEntryPairs].translation, translation);
+    strcpy(Entry->EntryPair[nEntryPairs].language, language);
+    Entry->nEntryPairs++;
+    (*nEntry)++;
+  }
+}
+
+void DisplayAllEntries(EntryTag Entry[], int nEntry)
 {
   int i = 0;
   int j = 0;
+
   for (i = 0; i < nEntry; i++)
   {
-    printf("%d: \n", Entry[i].EntryListKey);
-    for (j = 0; j < Entry[i].nEntryPairs; j++)
+    int nEntryPairs = Entry[i].nEntryPairs;
+    for (j = 0; j < nEntryPairs; j++)
     {
-      printf("%d: %s: %s\n", Entry[i].entries[j].EntryPairKey, Entry[i].entries[j].language, Entry[i].entries[j].translation);
-    }
-    printf("\n");
-  }
-  printf("\n");
-}
-
-void DisplayTranslations(EntryList *Entry, int nEntry)
-{
-  int i = 0;
-  for (i = 0; i < Entry[nEntry].nEntryPairs; i++)
-  {
-    printf("%d: %s: %s", Entry[nEntry].nEntryPairs, Entry[nEntry].entries[i].translation, Entry[nEntry].entries[i].language);
-    printf("\n");
-  }
-  printf("\n");
-}
-
-int SearchEntryPair(EntryList *Entry, String20 language, String20 translation, int *nEntry)
-{
-  int isValid = 0;
-  int entryNumber = *nEntry;
-  if (Entry->nEntryPairs == 0)
-  {
-    isValid = -1;
-  }
-  else
-  {
-    int i = 0;
-    int j = 0;
-    for (i = 0; i < entryNumber; i++)
-    {
-      for (j = 0; j < Entry[i].nEntryPairs; j++)
-      {
-        if (strcmp(language, Entry[i].entries[j].language) == 0 && strcmp(translation, Entry[i].entries[j].translation) == 0)
-          isValid = i;
-        else
-          isValid = -1;
-      }
+      printf("%s: %s\n", Entry[i].EntryPair[j].language, Entry[i].EntryPair[j].translation);
     }
   }
-  printf("\n");
-  return isValid;
-}
-
-void AddEntry(EntryList *Entry, int *nEntry)
-{
-  String20 language;
-  String20 translation;
-
-  EntryList *entry = &Entry[*nEntry];
-  int nEntryPairs = entry->nEntryPairs;
-
-  printf("Input Language: ");
-  scanf("%s", language);
-  printf("Input Translation: ");
-  scanf("%s", translation);
-  if (SearchEntryPair(Entry, language, translation, nEntry) == -1)
-  {
-    strcpy(entry->entries[nEntryPairs].language, language);
-    strcpy(entry->entries[nEntryPairs].translation, translation);
-    entry->entries[nEntryPairs].EntryPairKey = (entry->nEntryPairs + 1);
-    entry->EntryListKey = (*nEntry + 1);
-    entry->nEntryPairs++;
-    (*nEntry)++;
-    printf("\n");
-  }
-  else
-  {
-    printf("\nENTRY ALREADY EXISTS!\n");
-  }
-}
-
-void AddTranslation(EntryList *Entry, int *nEntry)
-{
-  String20 language;
-  String20 translation;
-
-  printf("Give a Language and Translation Pair:\n");
-  printf("Input Language: ");
-  scanf("%s", language);
-  printf("Input Translation: ");
-  scanf("%s", translation);
-  if (SearchEntryPair(Entry, language, translation, nEntry) == -1)
-  {
-    printf("You need to add entry first before adding translation\n\n");
-  }
-  else
-  {
-    int specificTranslation = SearchEntryPair(Entry, language, translation, nEntry);
-    DisplayTranslations(Entry, specificTranslation);
-    printf("Add a new Language and Translation Pair:\n");
-    printf("Input Language: ");
-    printf("Input Language: ");
-    scanf("%s", language);
-    printf("Input Translation: ");
-    scanf("%s", translation);
-    // strcpy(Entry[])
-  }
+  printf("\n\n");
 }
 
 int main()
 {
   int input = 0;
-  int nEntries = 0;
+  EntryTag Entries[MAXENTRIES] = {0};
+  int nEntry = 0;
 
-  EntryList Entry[MAXENTRY] = {0};
-
-  do
+  while (input != 3)
   {
-    printf("Translator:\n");
-    printf("1.) Translate Menu \n");
-    printf("2.) Manage Data \n");
-    printf("3.) Exit \n");
-    printf("Choose Option: ");
+    printf("Language Translator: \n");
+    printf("1. Manage Data\n");
+    printf("2. Translate Menu\n");
+    printf("3. Exit\n");
+    printf("Action: ");
     scanf("%d", &input);
+    printf("\n\n");
 
-    if (input == 2)
+    // Manage Data
+    if (input == 1)
     {
-      printf("\nGoing to Manage Data\n\n");
-      int input = 0;
-      do
+      while (input != 10)
       {
-        printf("Manage Data:\n");
-        printf("1.) Add Entry\n");
-        printf("2.) Add Translations\n");
-        printf("3.) Delete Entry\n");
-        printf("4.) Delete Translation\n");
-        printf("5.) Display All Entries\n");
-        printf("6.) Search Word\n");
-        printf("7.) Search Translation\n");
-        printf("8.) Export\n");
-        printf("9.) Import\n");
-        printf("10.) Exit\n");
-        printf("Choose Option: ");
+        printf("Manage Data: \n");
+        printf("1. Add Entry\n");
+        printf("2. Add Translations\n");
+        printf("3. Delete Entry\n");
+        printf("4. Delete Translations\n");
+        printf("5. Display All Entries\n");
+        printf("6. Search Word\n");
+        printf("7. Search Translations\n");
+        printf("8. Export\n");
+        printf("9. Import\n");
+        printf("10. Exit\n");
+        printf("Action: ");
         scanf("%d", &input);
-
+        printf("\n\n");
         if (input == 1)
         {
-          printf("Going to Add Entry\n");
-          AddEntry(Entry, &nEntries);
+          AddEntry(&Entries[nEntry], &nEntry, Entries);
         }
-        else if (input == 2)
+        if (input == 5)
         {
-          printf("Going to Add Translations\n");
-          AddTranslation(Entry, &nEntries);
+          DisplayAllEntries(Entries, nEntry);
         }
-        else if (input == 5)
-        {
-          printf("Displaying Entries\n");
-          printf("\n");
-          DisplayEntries(Entry, nEntries);
-        }
-        else if (input == 10)
-          printf("\nGoing Back to Main Menu\n");
-      } while (input != 10);
+      }
     }
-    if (input == 3)
-    {
-      printf("\nQuitting the Program\n");
-    }
-    else
-    {
-      printf("\nInvalid Option\n");
-    }
-  } while (input != 3);
 
-  return 0;
+    // Translate Menu
+    if (input == 2)
+    {
+    }
+  }
 }
---
+++ Headers.h
@@ -0,0 +1,18 @@
+#define MAXPAIRS 10
+#define MAXENTRIES 150
+
+typedef char String20[21];
+
+typedef struct
+{
+    String20 language;
+    String20 translation;
+} EntryPairTag;
+
+typedef struct
+{
+    int nEntryPairs;
+    EntryPairTag EntryPair[MAXPAIRS];
+} EntryTag;
+
+void AddEntry(EntryTag Entry, int *nEntry);
